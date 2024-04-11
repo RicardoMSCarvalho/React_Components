bcfeeae88f3420a64425fce9775cf24afc9c0113
RicardoMSCarvalho <114369188+RicardoMSCarvalho@users.noreply.github.com>
2024-04-11 17:48:09 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import List from "../List";
+import Heading from "../Heading";
+ReactDOM.render(
+  <div>
+    <Heading />
+    <List />
+  </div>,
+  document.getElementById("root")
+);
